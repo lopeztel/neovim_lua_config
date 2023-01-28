4a62a3381e38bca9e2cb997915dc77577cfbfc64
lopeztel <lopeztel.gmail.com>
2023-01-28 11:48:04 +0100

    Fixing null-ls configuration

--- lua/mason-config/init.lua
+++ lua/mason-config/init.lua
@@ -31,10 +31,26 @@ local cmp = require('cmp')
 -----------------------------------lspkind--------------------------------------
 local lspkind = require('lspkind')
 -----------------------------------null-ls--------------------------------------
+local null_ls = require('null-ls')
+local null_ls_sources = {
+  -- null_ls.builtins.diagnostics.markdownlint,
+  -- null_ls.builtins.formatting.markdownlint,
+  -- null_ls.builtins.formatting.stylua,
+  -- null_ls.builtins.formatting.clang_format,
+  -- null_ls.builtins.diagnostics.cmake_lint,
+  -- null_ls.builtins.formatting.cmake_format
+  null_ls.builtins.code_actions.gitsigns
+}
 require('mason-null-ls').setup({
+  ensure_installed = {"clang-format", "stylua", "cmake_lint", "cmake_format", "markdownlint"},
+  automatic_installation = true,
   automatic_setup = true,
-  ensure_installed = {"clang-format", "stylua"}
 })
+
+null_ls.setup{
+  sources = null_ls_sources,
+}
+require 'mason-null-ls'.setup_handlers()
 
 ----------------------------------autopairs-------------------------------------
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
@@ -132,20 +148,25 @@ local on_attach = function(client, bufnr)
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  local builtin = require('telescope.builtin')
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gd', builtin.lsp_definitions, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, bufopts)
+  -- vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'gI', builtin.lsp_implementations, bufopts)
   vim.keymap.set('n', '<leader>sh', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
   vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
   vim.keymap.set('n', '<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
+  vim.keymap.set('n', 'gs', builtin.lsp_type_definitions, bufopts)
   vim.keymap.set('n', 'gs', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', 'gR', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', 'gr', builtin.lsp_references, bufopts)
   vim.keymap.set('n', '<leader>cf', function()
     vim.lsp.buf.format { async = true }
   end, bufopts)
--- lua/nvim-notify-config/init.lua
+++ lua/nvim-notify-config/init.lua
@@ -3,3 +3,12 @@ require('notify').setup({
   render = "default", --default style rendering
 })
 
+-- NOTE: The following is added to suppress null-ls warnings about different client
+-- offset encodings
+local notify = vim.notify
+vim.notify = function(msg, ...)
+  if msg:match("warning: multiple different client offset_encodings") then
+    return
+  end
+  notify(msg, ...)
+end
