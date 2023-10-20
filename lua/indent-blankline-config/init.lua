7bc21dd8bb1bb97c94105ca38dbc2fd1072e40ff
lopeztel <lopeztel.gmail.com>
2023-10-20 16:59:31 +0200

    Updating to indent-blankline 3

--- lua/indent-blankline-config/init.lua
+++ lua/indent-blankline-config/init.lua
@@ -1,7 +1,1 @@
-require("indent_blankline").setup {
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
-    -- show_end_of_line = true,
-    use_treesitter_scope = true
-}
+require("ibl").setup()
